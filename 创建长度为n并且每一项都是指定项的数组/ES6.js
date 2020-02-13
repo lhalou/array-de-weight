f7537799a28ad7ad47ec831330dbc1eabe593988
lhalou <614124284@qq.com>
2020-02-13 15:54:02 +0800

    Update ES6.js

--- 创建长度为n并且每一项都是指定项的数组/ES6.js
+++ 创建长度为n并且每一项都是指定项的数组/ES6.js
@@ -1,10 +1,7 @@
 function x(n, fill) {
   var array = Array.from({ length: n });
   return array.map(v => fill);
+  //上面两句话等价于 rreturn Array.form({length:n}).fill(fill)
 }
 console.log(x(6, 6)); //[6, 6, 6, 6, 6, 6]
 
-function x(n, fill) {
-  return new Array(n + 1).join(fill).split("");
-}
-console.log(x(6, 6)); //["6", "6", "6", "6", "6", "6"]
